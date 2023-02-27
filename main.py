35cb7a99ee584019ac066e8a38e04a0ad8e5ce55
Michael Wang <113716608+pl244783@users.noreply.github.com>
2023-02-27 08:33:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 import cv2
 import numpy as np
 
-# Read image.
-img = cv2.imread('circle.png', cv2.IMREAD_COLOR)
 
-# Convert to grayscale.
+img = cv2.imread('circle.png')
+output = img.copy()
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Blur using 3 * 3 kernel.
-gray_blurred = cv2.blur(gray, (3, 3))
-
-# Apply Hough transform on the blurred image.
-detected_circles = cv2.HoughCircles(gray_blurred,
-                                    cv2.HOUGH_GRADIENT, 1, 100000, param1=50,
-                                    param2=30, minRadius=1, maxRadius=40)
-
-# Draw circles that are detected.
-if detected_circles is not None:
-
-    # Convert the circle parameters a, b and r to integers.
-    detected_circles = np.uint16(np.around(detected_circles))
-
-    for pt in detected_circles[0, :]:
-        a, b, r = pt[0], pt[1], pt[2]
-
-        # Draw the circumference of the circle.
-        cv2.circle(img, (a, b), r, (0, 255, 0), 2)
-
-        # Draw a small circle (of radius 1) to show the center.
-        cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
-        cv2.imshow("Detected Circle", img)
-        cv2.waitKey(0)
-
-# cv2.imshow("Detected Circle", img)
-# cv2.waitKey(0)
+# detect circles in the image
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100000)
+# ensure at least some circles were found
+if circles is not None:
+    # convert the (x, y) coordinates and radius of the circles to integers
+    circles = np.round(circles[0, :]).astype("int")
+    # loop over the (x, y) coordinates and radius of the circles
+    for (x, y, r) in circles:
+        # draw the circle in the output image, then draw a rectangle
+        # corresponding to the center of the circle
+        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    # show the output image
+    cv2.imshow("output", np.hstack([img, output]))
+    cv2.waitKey(0)
