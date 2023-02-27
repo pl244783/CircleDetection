e36e3941e7952f4e5c6a1df4c7d2ce71c282a257
Michael Wang <113716608+pl244783@users.noreply.github.com>
2023-02-27 08:31:26 -0500

    I want to die

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('path/to/video')
+# Read image.
+img = cv2.imread('circle.png', cv2.IMREAD_COLOR)
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=30, minRadius=0, maxRadius=0)
+# Convert to grayscale.
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        if circles is not None:
-            circles = np.round(circles[0, :]).astype("int")
-            for (x, y, r) in circles:
-                cv2.circle(frame, (x, y), r, (0, 255, 0), 2)
+# Blur using 3 * 3 kernel.
+gray_blurred = cv2.blur(gray, (3, 3))
 
-        cv2.imshow('Frame', frame)
+# Apply Hough transform on the blurred image.
+detected_circles = cv2.HoughCircles(gray_blurred,
+                                    cv2.HOUGH_GRADIENT, 1, 100000, param1=50,
+                                    param2=30, minRadius=1, maxRadius=40)
 
-        if cv2.waitKey(25) & 0xFF == ord('
+# Draw circles that are detected.
+if detected_circles is not None:
+
+    # Convert the circle parameters a, b and r to integers.
+    detected_circles = np.uint16(np.around(detected_circles))
+
+    for pt in detected_circles[0, :]:
+        a, b, r = pt[0], pt[1], pt[2]
+
+        # Draw the circumference of the circle.
+        cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+
+        # Draw a small circle (of radius 1) to show the center.
+        cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+        cv2.imshow("Detected Circle", img)
+        cv2.waitKey(0)
+
+# cv2.imshow("Detected Circle", img)
+# cv2.waitKey(0)
